881d9c5eb8fa5a8dc1685aa0c73fce1c553603bb
Bryan Oswaldo Juanacio Lalangui <bryan.juanacio@kushkipagos.com>
2022-09-27 15:32:11 -0500

    Extra Files

--- App.tsx
+++ App.tsx
@@ -1,19 +1,20 @@
-import React from "react";
-import { SafeAreaView, StatusBar, useColorScheme } from "react-native";
+import React from 'react';
+import {SafeAreaView, StatusBar, useColorScheme} from 'react-native';
 
-import { Home } from "./src/screens/home";
+import {Home} from './src/screens/home';
 
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
 const App = () => {
-  const isDarkMode = useColorScheme() === "dark";
+  const isDarkMode = useColorScheme() === 'dark';
 
   const Stack = createNativeStackNavigator();
+
   return (
     <NavigationContainer>
-      <SafeAreaView style={{ backgroundColor: "red", flex: 1 }}>
-        <StatusBar barStyle={isDarkMode ? "light-content" : "dark-content"} />
+      <SafeAreaView style={{backgroundColor: 'red', flex: 1}}>
+        <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
         <Stack.Navigator initialRouteName="Home">
           <Stack.Screen name="Home" component={Home} />
         </Stack.Navigator>
